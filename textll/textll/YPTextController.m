84ee447554738dcd37e19880ebecdb4291815520
殷云鹏 <1145950939@qq.com>
2017-06-21 16:18:44 +0800

    创建功能view

--- textll/textll/YPTextController.m
+++ textll/textll/YPTextController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UIView *view = [[UIView alloc] init];
+    [self.view addSubview:view];
 }
 
 - (void)didReceiveMemoryWarning {
