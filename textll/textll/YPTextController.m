478d393ba2853f003452c6166aa906d8a60aaee0
殷云鹏 <1145950939@qq.com>
2017-06-21 16:12:18 +0800

    kkkkkkkk

---
+++ textll/textll/YPTextController.m
@@ -0,0 +1,37 @@
+//
+//  YPTextController.m
+//  textll
+//
+//  Created by 殷云鹏 on 17/6/21.
+//  Copyright © 2017年 XinZhongXin. All rights reserved.
+//
+
+#import "YPTextController.h"
+
+@interface YPTextController ()
+
+@end
+
+@implementation YPTextController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
